3fdf65f5dfe1e8e9b1b7f3885f2f33964f3dc764
Zachary Jordan <zacharyjordan@my.whccd.edu>
2025-10-22 20:43:49 -0700

    add if else for grades

---
+++ script.js
@@ -0,0 +1,14 @@
+let myGrade = 0;
+
+if (myGrade >= 90 ) {
+    console.log("Great Job, You have an A");
+} else if (myGrade >= 80 && myGrade < 90) {
+    console.log("You have a B, Thats good but not great");
+} else if (myGrade >= 70 && myGrade < 80) {
+    console.log("You have a C. You're cutting it close");
+} else if (myGrade >= 60 && myGrade < 70) {
+    console.log("Your grade is a D, YOURE AT RISK OF FAILING");
+} else {
+    console.log("You're failing. you have an F. do your work and turn it in!");
+}
+
